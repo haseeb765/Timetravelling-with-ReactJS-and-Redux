15ecc093c1a49fbbf60e6f1dd224ddb505cb60a2
LordLalwani <Deepplalwani@hotmail.com>
2020-04-13 16:11:34 +1000

    Changed bg color + added two buttons for shopping list

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { TextField } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  root: {
+    margin: "0px 10px 10px 0px"
+  }
+});
 
 function App() {
+  const classes = useStyles();
   return (
     <div className="App">
       <header className="App-header">
@@ -10,14 +19,23 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="Shopping-list-container">
+          <TextField
+          classes={{
+            root: classes.root
+          }}
+            id="standard-basic"
+            variant="outlined"
+            label="Add to list"
+            placeholder="Milk"
+          />
+          <TextField
+            id="standard-basic"
+            variant="outlined"
+            label="Remove from list"
+            placeholder="Bananas"
+          />
+        </div>
       </header>
     </div>
   );
