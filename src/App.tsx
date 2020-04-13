f4a42d03dc55e078d2cf2daba099bd2a5b201e2a
LordLalwani <Deepplalwani@hotmail.com>
2020-04-13 20:28:02 +1000

    Set up event handling, ready to involve redux

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,26 @@ import logo from './logo.svg';
 import './App.css';
 import { TextField } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
+import { addToList, RemoveFromList } from "./utils/listUtils"
 
 const useStyles = makeStyles({
   root: {
     margin: "0px 10px 10px 0px"
   }
 });
+
+const handleKeyPress = (e: React.KeyboardEvent): void => {
+  if (!!e.keyCode && e.keyCode === 13) {
+    const eventTarget = e.target as HTMLTextAreaElement;
+    const eventId = eventTarget.id;
+
+    if (eventId === "textField-add") {
+      addToList(eventTarget.value);
+    } else if (eventId === "textField-remove") {
+      RemoveFromList(eventTarget.value);
+    }
+  }
+}
 
 function App() {
   const classes = useStyles();
@@ -21,19 +35,24 @@ function App() {
         </p>
         <div className="Shopping-list-container">
           <TextField
-          classes={{
-            root: classes.root
-          }}
-            id="standard-basic"
+            classes={{
+              root: classes.root
+            }}
+            id="textField-add"
             variant="outlined"
             label="Add to list"
             placeholder="Milk"
+            onKeyUp={(e) => handleKeyPress(e)}
           />
           <TextField
-            id="standard-basic"
+            classes={{
+              root: classes.root
+            }}
+            id="textField-remove"
             variant="outlined"
             label="Remove from list"
             placeholder="Bananas"
+            onKeyUp={(e) => handleKeyPress(e)}
           />
         </div>
       </header>
---
+++ src/utils/listUtils.ts
@@ -0,0 +1,7 @@
+export const addToList = (item: string) => { 
+    console.log(item);
+}
+
+export const RemoveFromList = (item: string) => { 
+    console.log(item);
+}
